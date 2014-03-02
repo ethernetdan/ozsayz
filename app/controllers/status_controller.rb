1cf43659e1adec6623444de7afb2d31dda2c22d1
Dan Gillespie <danielg@umd.edu>
2014-03-02 18:00:51 -0500

    added pass

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,16 +1,18 @@
 class StatusController < ApplicationController
 	def update
-		s = Status.last
-		if s == nil
-			s = Status.new
+		if params[:pass] == "hellocollider!!!"
+			s = Status.last
+			if s == nil
+				s = Status.new
+			end
+			if params[:open] == "1"
+				s.open = true
+			elsif params[:open] == "0"
+				s.open = false
+			end
+			s.save()
+			render :text => s.open
 		end
-		if params[:open] == "1"
-			s.open = true
-		elsif params[:open] == "0"
-			s.open = false
-		end
-		s.save()
-		render :text => s.open
 	end
 
 	def read
